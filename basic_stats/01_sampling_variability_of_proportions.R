808518d7a8cb6979944d68837b3fd34d75637b69
Zaw Myo Tun <zawmyotun11@gmail.com>
2023-11-03 14:32:49 +0800

    basic stats comparing two prop pptx

--- basic_stats/01_sampling_variability_of_proportions.R
+++ basic_stats/01_sampling_variability_of_proportions.R
@@ -1,75 +1,73 @@
 library(tidyverse)
 
+# Construct an example data
 # Systolic blood pressure of everyone in village A. (n = 1000)
 set.seed(111)
 dat <- data.frame(serno = 1:1e3,
-                  sbp = rnorm(1e3, 120, 25)) |> 
+                  sbp = rnorm(1e4, 120, 25)) |> 
   mutate(
-    hypertension = if_else(sbp >= 140, "HT", "Non-HT")
+    hypertension = if_else(sbp >= 140, "Hypertensive", "Normotensive")
   )
 
-ggplot(dat, aes(x = sbp)) +
-  geom_histogram(binwidth = 10) +
-  labs(title = "Systolic blood pressure of village A")
-
-ggplot(dat, aes(x = hypertension, y = ..count../sum(..count..))) +
-  geom_bar() +
-  scale_y_continuous(limits = c(0, 1), labels = scales::label_percent()) +
-  labs(title = "Prevalence of hypertension in village A.",
-       x = "Percentage", y = NULL)
+hist(dat$sbp)
 
 ht <- dat |>
   count(hypertension = sbp >= 140) |> 
   mutate(prop = n/sum(n))
 
-# Let's say we don't know the true proportion and want to estimate it from a sample.
-# Select a random sample of 150 people
-dat_samp <- dat |> 
-  sample_n(150)
+ht
 
-hist(dat_samp$sbp)
+ggplot(dat, aes(x = hypertension, y = after_stat(count)/sum(after_stat(count)), fill = hypertension)) +
+  geom_bar(show.legend = FALSE) +
+  scale_y_continuous(limits = c(0, 1), labels = scales::label_percent()) +
+  labs(title = "Prevalence of hypertension in village A.",
+       x = "Percentage", y = NULL)
+
+
+# Let's draw a random sample
+dat_samp <- sample_n(dat, 100)
 
 dat_samp |>
   count(hypertension) |> 
-  mutate(prop = n/sum(n)) |> 
-  filter(hypertension == "HT") |> 
-  pull(prop)
+  mutate(prop = n/sum(n))
 
-# Repeat the sampling process 500 times
+# Construct my own functions
+# Standard error
 
-# First create a function to do the sampling
+get_se <- function(p, n) {
+  sqrt(p*(1-p)/n)
+}
+
+get_se(0.23, 100)
+
+get_95ci <- function(p, n) {
+  ll <- p - 1.96 * get_se(p = p, n = n)
+  ul <- p + 1.96 * get_se(p = p, n = n)
+  c(ll, ul)
+}
+
+ci <- get_95ci(0.23, 100) |> round(2)
+cat(0.23, "(95% CI:", ci[1], ",", ci[2], ")")
+
+# CI using prop.test()
+ht_prop <- table(dat_samp$hypertension)
+ht_prop
+prop.test(ht_prop)
+
+
+
+
+# Construct sampling distribution
 get_ht_prev_sample <- function(data, n) {
-  data |> 
-    sample_n(n) |> 
-    count(hypertension) |> 
-    mutate(prop = n/sum(n)) |> 
-    filter(hypertension == "HT") |> 
-    pull(prop)
+  dat <- sample_n(data, n)
+  ht_prev <- table(dat$hypertension) |> prop.table()
+  ht_prev["Hypertensive"] |> unname()
 }
 
-get_ht_prev_sample(dat, 150)
-
-ht_prev <- replicate(200, get_ht_prev_sample(dat, 150))
-quantile(ht_prev, probs = c(0.025,  0.975))
-
-ggplot(dat_ht_prev_samp, aes(x = ht_prev)) +
-  geom_histogram(bins = 10)
-
-# Estimate standard deviation
-
-ss <- seq(10, 1000, 10)
+get_ht_prev_sample(dat, 100)
+dat_rep <- replicate(3000, get_ht_prev_sample(dat, 100))
 
 
-
-
-
-get_ci <- function(data, var) {
-  
-}
-
-
-
-
-
-
-
+data.frame(dat_rep) |> 
+  ggplot(aes(x = dat_rep)) +
+  geom_histogram(bins = 30)
